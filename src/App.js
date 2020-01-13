a55250f8bc633c0c6cdbf0539935cf160ca709aa
MitchellMarkGeorge <mitchellmmarkgeorge@gmail.com>
2020-01-13 11:31:03 -0500

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Terminal from 'terminal-in-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className="App">
+        <Terminal
+          color='green'
+          backgroundColor='black'
+          barColor='black'
+          style={{ fontWeight: "bold", fontSize: "1em" }}
+          commands={{
+            'open-google': () => {window.open('https://www.google.com/', '_blank')},
+            // showmsg: this.showMsg,
+            // popup: () => alert('Terminal in React')
+          }}
+          descriptions={{
+            'open-google': 'opens google.com',
+            showmsg: 'shows a message',
+            alert: 'alert', popup: 'alert'
+          }}
+          msg='You can write anything here. Example - Hello! My name is Foo and I like Bar.'
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
